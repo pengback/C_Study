81a34ba238cbdcd0293c770aa3f426f1c91ce1b9
aliber <ypBack@126.com>
2022-04-27 14:32:03 +0800

    Makefile学习

---
+++ make_file/head.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_info(char* str)
+{
+    printf("%s\n", str);
+}
---
+++ make_file/main.c
@@ -0,0 +1,7 @@
+#include "head.h"
+
+int main() 
+{
+    print_info("I am a bbbbbo");
+    return 0;
+}
